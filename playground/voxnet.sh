8bc56e4ec35a57807a8bfb7e080639952c373c47
Kelechi Ikegwu <kelechi.ikegwu@gmail.com>
2017-04-09 00:29:22 -0500

    Sample script of how you would convert .stl files to .boxnet

---
+++ playground/voxnet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Simple Script to to convert test files to binvox
+
+BINVOX=~/Downloads/binvox  # Change to path of binvox
+
+$BINVOX decorated_cup_1.stl
+$BINVOX decorated_cup_2.stl
+$BINVOX mugV02_1x.stl
+$BINVOX simple_cup.stl
+$BINVOX test2.stl
+$BINVOX test.stl
