67bd8c5adc41883d79e1610826faa0a08476b4b3
Kelechi Ikegwu <kelechi.ikegwu@gmail.com>
2017-04-09 11:59:42 -0500

    Made a little more dynamic

--- playground/voxnet.sh
+++ playground/voxnet.sh
@@ -3,7 +3,9 @@
 # Simple Script to to convert test files to binvox
 
 BINVOX=~/Downloads/binvox  # Change to path of binvox
-
+DATA=/media/jarvis/pubData  # Expression should list out all model files
+for filename in $DATA; do
+	$BINVOX filename
 $BINVOX decorated_cup_1.stl
 $BINVOX decorated_cup_2.stl
 $BINVOX mugV02_1x.stl
