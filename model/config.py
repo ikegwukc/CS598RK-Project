64ca0ae7726591351919af56397055d264c4d543
Kelechi Ikegwu <kelechi.ikegwu@gmail.com>
2017-04-09 00:08:51 -0500

    Inital Structure for model

---
+++ model/config.py
@@ -0,0 +1,6 @@
+'''
+dataset_path: unix path like expression that points to ALL .binvox files
+params_path: ???
+'''
+dataset_path = "/home/jarvis/GoogleDrive/UIUC_Grad/coursework/CS_598-RK/project/playground/*.binvox"
+params_path = ""
