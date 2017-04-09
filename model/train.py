d85feb42d520f48856bb8066624746c779d380e3
Kelechi Ikegwu <kelechi.ikegwu@gmail.com>
2017-04-09 11:56:40 -0500

    Add Progressbar, fixed initialization, dynamically created missing dirs

--- model/train.py
+++ model/train.py
@@ -1,25 +1,53 @@
+
+# coding: utf-8
+
+# In[1]:
+
 import tensorflow as tf
 import numpy as np
 import model
 import util
 import dataset
 import time
+import os
+import progressbar
 
+
+# ### Setting params, dirs, and loading data
+
+# In[2]:
+
+paths = ['./out/', './params']
+for path in paths:
+    if not os.path.exists(path):
+        os.makedirs(path)
+                
 data = dataset.read()
-
 batch_size = 100
 learning_rate = 0.0001
 beta1 = 0.5
 z_size = 100
 save_interval = 1
 
+
+
+# ###  Creating Placeholders
+
+# In[3]:
+
 x = tf.placeholder(tf.float32, [batch_size, 32, 32, 32, 1])
 z = tf.placeholder(tf.float32, [batch_size, z_size])
 train = tf.placeholder(tf.bool)
 
+
+# ### Create Generator and Discriminator Object
+# #### Setting up labels, loss functions, other parameters
+
+# In[4]:
+
+
 G = model.Generator(z_size)
 D = model.Discriminator()
-
 x_ = G(z)
 y_ = D(x_, train)
 
@@ -42,14 +70,28 @@ opt_D = tf.train.AdamOptimizer(learning_rate, beta1).minimize(loss_D, var_list=v
 
 saver = tf.train.Saver()
 
+
+# ### Setting up Progressbar
+
+# In[5]:
+
+progress = progressbar.ProgressBar()
+
+
+# ### Running Voxel-dcgan
+
+# In[6]:
+
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 
 with tf.Session(config=config) as sess:
-    sess.run(tf.initialize_all_variables())
+    
+    #init = 
+    sess.run(tf.initialize_all_variables(), feed_dict={train:True})
     total_batch = data.train.num_examples / batch_size
 
-    for epoch in xrange(1, 51):
+    for epoch in progress(xrange(1, 51)):  # Repeat this process 50 times
 
         for i in xrange(total_batch):
             voxels = np.expand_dims(data.train.next_batch(batch_size), 4)
@@ -70,7 +112,14 @@ with tf.Session(config=config) as sess:
 
         for j, v in enumerate(voxels[:5]):
             v = v.reshape([32, 32, 32]) > 0
+
             util.save_binvox(v, "out/epoch{0}-{1}.binvox".format(epoch, j))
 
         if epoch % save_interval == 0:
             saver.save(sess, "params/epoch{0}.ckpt".format(epoch))
+
+
+# In[ ]:
+
+#
+
